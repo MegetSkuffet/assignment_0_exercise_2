65b7d7d0017ac868134e90f2fefda00c9538905b
Malte Alexander Koefoed Schalck <93313173+MegetSkuffet@users.noreply.github.com>
2022-09-05 21:26:24 +0200

    created userinterface for LeapYear

--- exercise_2/Program.cs
+++ exercise_2/Program.cs
@@ -1,16 +1,25 @@
 ﻿namespace exercise0;
 
 
+
 public class LeapYear{
 
-    static void Main()
-{
-}
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("write a year to find out if it is a leapyear!");
+        var year = new LeapYear();
+        year.IsLeapYear(Convert.ToInt32(Console.ReadLine()));
+    }
     public string IsLeapYear(int year)
     {
-        if(year%4==0)  return "yay";
-        else return "nay";
+        if(year%4==0) {
+            Console.WriteLine("yay") ;
+            return "yay";
+        }
+        else {
+            Console.WriteLine("nay");
+            return "nay";
+        }
+        
     }
-
-
 }
